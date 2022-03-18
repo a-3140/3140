c52f82aeb83912815095faff9abd07bc0a262c57
a-3140 <aatienza.3140@gmail.com>
2022-03-18 23:43:17 +0800

    feature - add breadcrumb

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import BreadCrumb from '@/components/blog/Breadcrumb.vue'
 import { data } from '@/data/data'
 
 const { id } = useRouter().currentRoute.value.params;
@@ -11,10 +12,18 @@ console.log('content', content);
 <template>
     <div class="container">
         <div
-            class="w-3/4 mt-24 max-w-5xl mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl text-center"
+            class="w-3/4 max-w-5xl mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl text-center"
         >
-            <h1 class="text-3xl font-bold mb-4">Test Title Here: {{ id }}</h1>
-            <p class="text-gray-500 text-lg text-left leading-8 font-light">{{ content.content }}</p>
+            <bread-crumb />
+
+            <div class="container">
+                <div class="mt-8 max-w-5xl mx-auto pb-28 xl:max-w-6xl text-center">
+                    <h1 class="text-3xl font-bold mb-4">Test Title Here</h1>
+                    <p
+                        class="text-gray-500 text-lg text-left leading-8 font-light"
+                    >{{ content.content }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
