bf6aa9473118a507de2b2a474fcd21138d159111
a-3140 <aatienza.3140@gmail.com>
2022-03-19 02:02:43 +0800

    feature - add bg
    adjust spacings

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,12 +1,23 @@
-<script setup lang="ts">
+<script lang="ts">
 import Header from '@/components/layout/Header.vue';
+import { defineComponent } from 'vue';
+import bg from '@/assets/bg.jpg';
+
+export default defineComponent({
+    components: { Header },
+    data() {
+        return {
+            image: `url(${bg})`,
+        }
+    }
+})
 
 </script>
 
 <template>
-    <div>
+    <div id="wrapper" v-bind:style="{ 'background-image': image }">
         <Header />
-        <div class="container mx-auto mt-4">
+        <div class="container mx-auto content-wrapper">
             <slot />
         </div>
     </div>
@@ -16,5 +27,14 @@ import Header from '@/components/layout/Header.vue';
 <style>
 #content {
     display: flex;
+}
+#wrapper {
+    min-height: 100vh;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+}
+.content-wrapper {
+    background: rgba(255, 255, 255, 0.9);
 }
 </style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -6,13 +6,12 @@ import { data } from '@/data/data'
 const { id } = useRouter().currentRoute.value.params;
 
 const content = data.filter(item => item.id === id)[0];
-console.log('content', content);
 </script>
 
 <template>
-    <div class="container">
+    <div class="container pt-4">
         <div
-            class="w-3/4 max-w-5xl mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl text-center"
+            class="w-3/4 max-w-5xl mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl text-center min-h-screen"
         >
             <bread-crumb />
 
