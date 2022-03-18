f1f76eff965b0ead65beaa3c05a80c182697b582
a-3140 <aatienza.3140@gmail.com>
2022-03-18 17:27:14 +0800

    feature - Add css for blog render

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -13,67 +13,14 @@ export default defineComponent({
 </script>
 
 <template>
-    <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto flex flex-col">
-            <div class="lg:w-4/6 mx-auto">
-                <div class="rounded-lg h-64 overflow-hidden">
-                    <img
-                        alt="content"
-                        class="object-cover object-center h-full w-full"
-                        src="https://dummyimage.com/1200x500"
-                    />
-                </div>
-                <div class="flex flex-col sm:flex-row mt-10">
-                    <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                        <div
-                            class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400"
-                        >
-                            <svg
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                class="w-10 h-10"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-                                <circle cx="12" cy="7" r="4" />
-                            </svg>
-                        </div>
-                        <div class="flex flex-col items-center text-center justify-center">
-                            <h2
-                                class="font-medium title-font mt-4 text-gray-900 text-lg"
-                            >Phoebe Caulfield</h2>
-                            <div class="w-12 h-1 bg-red-500 rounded mt-2 mb-4"></div>
-                            <p
-                                class="text-base"
-                            >Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
-                        </div>
-                    </div>
-                    <div
-                        class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left"
-                    >
-                        <p
-                            class="leading-relaxed text-lg mb-4"
-                        >Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
-                        <a class="text-red-500 inline-flex items-center">
-                            Learn More
-                            <svg
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                class="w-4 h-4 ml-2"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M5 12h14M12 5l7 7-7 7" />
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-            </div>
+    <div class="container">
+        <div
+            class="w-3/4 mt-24 max-w-5xl mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl text-center"
+        >
+            <h1 class="text-3xl font-bold mb-4">Test Title Here</h1>
+            <p
+                class="text-gray-500 text-lg text-left leading-8 font-light"
+            >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
-    </section>
+    </div>
 </template>
