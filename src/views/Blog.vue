fc2792a32cf292e7ba780fb35305cd043b9bfefe
a-3140 <aatienza.3140@gmail.com>
2022-03-19 16:15:43 +0800

    feature - show not found when path doesn't exist on blogs

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import BreadCrumb from '@/components/blog/Breadcrumb.vue'
+import NotFound from '@/views/NotFound.vue'
 import { data } from '@/data/data'
 
 const { id } = useRouter().currentRoute.value.params;
@@ -9,13 +10,13 @@ const content = data.filter(item => item.id === id)[0];
 </script>
 
 <template>
-    <div class="container pt-4">
+    <div class="mx-auto pt-4">
         <div
-            class="w-3/4 max-w-5xl mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl text-center min-h-screen"
+            class="max-w-5xl mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl text-center min-h-screen"
         >
             <bread-crumb />
 
-            <div class="container">
+            <div v-if="content" class="container">
                 <div class="mt-8 max-w-5xl mx-auto pb-28 xl:max-w-6xl text-center">
                     <h1 class="text-3xl font-bold mb-4">Test Title Here</h1>
                     <p
@@ -23,6 +24,8 @@ const content = data.filter(item => item.id === id)[0];
                     >{{ content.content }}</p>
                 </div>
             </div>
+
+            <not-found v-else />
         </div>
     </div>
 </template>
