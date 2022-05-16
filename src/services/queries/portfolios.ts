d33b26dc5f2819412ba79139af590cc3f06ebbbe
a-3140 <aatienza.3140@gmail.com>
2022-05-16 19:09:32 +0800

    tech - order portfolio by their order id

--- src/services/queries/portfolios.ts
+++ src/services/queries/portfolios.ts
@@ -1,4 +1,6 @@
-import { collection, doc, getDocs, QueryDocumentSnapshot } from 'firebase/firestore';
+import {
+    collection, doc, getDocs, orderBy, query, QueryDocumentSnapshot
+} from 'firebase/firestore';
 
 import { db } from '@/firebase';
 import { FetchCollectionParams, PortfolioSummary } from '@/types/queries';
@@ -6,7 +8,9 @@ import { FetchCollectionParams, PortfolioSummary } from '@/types/queries';
 export const fetchPortfolioSummaries = async (
   params: FetchCollectionParams
 ): Promise<PortfolioSummary[]> => {
-  const querySnapshot = await getDocs(collection(db, params.collection));
+  const ref = collection(db, params.collection);
+  const q = query(ref, orderBy("order_id", "asc"));
+  const querySnapshot = await getDocs(q);
 
   const data = querySnapshot.docs.map((doc: QueryDocumentSnapshot) => {
     const { title, description, github, link } = doc.data();
