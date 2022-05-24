59ed65d7be9062a5ba59a1ee565eb2c0c0462c5a
a-3140 <aatienza.3140@gmail.com>
2022-05-24 13:51:49 +0800

    ui - change collection header design

--- src/components/articles/ContentHeader.vue
+++ src/components/articles/ContentHeader.vue
@@ -8,16 +8,20 @@ const contentHeader = defineProps<ContentHeader>();
 </script>
 
 <template>
-  <header
+  <div
     class="pt-16 mb-6 pb-2 sm:mb-10 sm:pb-6 border-b-2 border-neutral-200 text-gray-50"
   >
-    <h1 class="mb-4 text-3xl sm:text-4xl font-extrabold animate-fade-in-down">
+    <h1
+      class="mb-4 text-3xl sm:text-4xl font-extrabold animate-fade-in-down font-mono text-green-400"
+    >
       {{ contentHeader.title }}
     </h1>
-    <p class="text-lg text-gray-300 animate-fade-in-down">
+    <p
+      class="mt-6 text-gray-500 md:text-left tracking-wider max-w-xl text-md animate-fade-in-right"
+    >
       {{ contentHeader.description }}
     </p>
-  </header>
+  </div>
 </template>
 
 <style></style>
