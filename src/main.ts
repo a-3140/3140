9beb399a08c454ec1e40b8c87cf6bb2b01d0eee2
a-3140 <aatienza.3140@gmail.com>
2022-06-02 14:45:16 +0800

    tech: Add gtagging
    Add vue-gtag lib

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './index.css';
 import 'flowbite';
 
 import { createApp } from 'vue';
+import VueGtag from 'vue-gtag';
 
 import App from './App.vue';
 import router from './router';
@@ -9,4 +10,17 @@ import router from './router';
 const app = createApp(App);
 
 app.use(router);
+
+app.use(
+  VueGtag,
+  {
+    appName: "Personal Website",
+    pageTrackerScreenviewEnabled: true,
+    config: {
+      id: "G-GZKKR415PF",
+    },
+  },
+  router
+);
+
 app.mount("#app");
