d78bf7ad6f0531f998a8c25df62aa249f49a89c4
a-3140 <aatienza.3140@gmail.com>
2022-06-09 23:13:39 +0800

    ui: Fix landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -3,30 +3,27 @@ import Github from "./buttons/Github.vue";
 </script>
 
 <template>
-  <div class="container fullscreen mx-auto">
-    <main
-      class="h-5/6 flex flex-col justify-center px-9 text-center items-center sm:items-start"
-    >
+  <div class="container h-screen mx-auto">
+    <main class="h-5/6 flex flex-col justify-center px-9 text-left items-start">
       <div
-        class="tracking-tighter text-5xl max-w-2xl font-mono text-green-400 animate-fade-in-up"
+        class="tracking-tighter md:text-5xl text-4xl max-w-xl font-mono text-green-400 animate-fade-in-up"
       >
         <span>a.atienza </span>
         <span class="text-green-800 block sm:inline-block"> web developer</span>
       </div>
       <div
-        class="mt-6 text-gray-500 md:text-left tracking-wider max-w-xl text-md animate-fade-in-right"
+        class="mt-6 text-gray-500 tracking-wider max-w-xl text-sm md:text-md animate-fade-in-right text-left"
       >
         Software developer based from the Philippines. Expertise on fullstack
         web-development using different stacks with internal and client-facing
         experience. Currently working in Trevipay which is based in Australia.
       </div>
-      <Github class="mt-8 animate-fade-in" link="https://github.com/a-3140" />
+      <Github
+        class="place-content-start mt-8 animate-fade-in"
+        link="https://github.com/a-3140"
+      />
     </main>
   </div>
 </template>
 
-<style scoped>
-.fullscreen {
-  height: calc(100vh - 68px);
-}
-</style>
+<style scoped></style>
